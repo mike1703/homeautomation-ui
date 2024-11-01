d60d27fc9fc145a4cd0a73ee21cd7ecfc6e0a544
Michael Kerscher <michael@kerscher-michael.de>
2024-11-01 20:23:56 +0100

    use vite-plugin-vuetify and get tree-shaking

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 
 const app = createApp(App);
 
 const vuetify = createVuetify({
-  components,
-  directives,
   theme: {
     defaultTheme: 'dark',
   },
