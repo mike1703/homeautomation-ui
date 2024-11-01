558234387992262f59f34007503741db3f5894b9
Michael Kerscher <michael@kerscher-michael.de>
2024-11-01 08:05:46 +0100

    reproducible asset names in build process

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 });
