a79ba339f4471871c1182b5d50ba827feebb73b8
Michael Kerscher <michael@kerscher-michael.de>
2024-11-01 20:01:42 +0100

    always show heating thumb-label

--- src/components/HeatingDevice.vue
+++ src/components/HeatingDevice.vue
@@ -9,8 +9,8 @@ function update(new_value: number) {
 </script>
 
 <template>
-  <v-slider min="15" max="30" step="0.5" @update:model-value="update" v-model="device.set_point_value" thumb-label
-    color="primary">
+  <v-slider min="15" max="30" step="0.5" @update:model-value="update" v-model="device.set_point_value"
+    thumb-label="always" color="primary">
     <template v-slot:append>
       <v-chip>{{ device.temperature_value }} °C</v-chip> {{ device.name }}
     </template>
